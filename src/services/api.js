4188ca802ad97a4ee553ac956c4f4afcba14e0c2
Leo <leo@Leos-iMac.local>
2020-05-26 20:14:54 -0300

    removed key

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const api = axios.create({
   baseURL: 'https://api.fortnitetracker.com/v1/profile/pc',
-  headers: { 'TRN-Api-Key': '1d89d312-8ad0-4d26-9efa-12f7ebc61482' }
+  headers: { 'TRN-Api-Key': 'your_api_key_here' }
 })
 
 module.exports = api
